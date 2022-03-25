22a1353fc9c5585f2b50735cad94b7171d16217d
ysshen <hustshenyang@126.com>
2022-03-25 08:13:17 +0800

    feat: 添加AT指令处理

--- vendor/826x_ser2ble/ser2ble.c
+++ vendor/826x_ser2ble/ser2ble.c
@@ -11,6 +11,8 @@
  *           
  *******************************************************************************************************/
 #include "../../proj/tl_common.h"
+#include "../../proj/common/string.h"
+#include "../../proj_lib/ble/ll/ll.h"
 #include "ys_uart.h"
 #include "ys_rom.h"
 #include "ble_drv.h"
@@ -25,10 +27,16 @@ MYFIFO_INIT(uart_rxfifo, UART_FIFO_SIZE, UART_FIFO_NUM);
 MYFIFO_INIT(uart_txfifo, UART_FIFO_SIZE, UART_FIFO_NUM);
 
 
+static u32 key_tick = 0;
+static u8 *sertx_ptr = NULL;
+
+int ser2ble_at_process(u8 *cmd, u8 len);
+
 void ser2ble_init(void)
 {
 	ble_app_init();
 	ys_uart_init(device_config.baudrate);
+	sertx_ptr = ys_uart_get_txaddr();
 }
 
 _attribute_ram_code_ void ser2ble_process(void)
@@ -41,9 +49,14 @@ _attribute_ram_code_ void ser2ble_process(void)
 			if(ble_nus_send_data(fp+2, fp[0]) == 0)
 				my_fifo_pop(&uart_rxfifo);
 			bls_pm_setManualLatency(0);
-		}else{
-
+		}else if(fp[0] >= 4 && fp[2] == 'A' && fp[3] == 'T'){
+			ser2ble_at_process(fp+4, fp[0]-2);
 		}
+	}
+
+	if(key_tick && clock_time_exceed(key_tick, 3000000)){
+		key_tick = 0;
+		YS_LOG("long pressed");
 	}
 }
 
@@ -92,5 +105,35 @@ int ble_nus_gpio_handler(u8 data)
 int ser2ble_cfg_key_handler(u8 val)
 {
 	YS_LOG("key val %d", val);
+	if(val == 0){
+		key_tick = (clock_time() | 1);
+	}else if(key_tick){
+		key_tick = 0;
+		device_config.baudrate++;
+		if(device_config.baudrate > UART_BAUD_230400)
+			device_config.baudrate = UART_BAUD_9600;
+		ys_uart_set_baud(device_config.baudrate);
+		ys_rom_save_device_config(&device_config);
+		YS_LOG("short pressed");
+	}
+
 	return 0;
+}
+
+int ser2ble_at_process(u8 *cmd, u8 len)
+{
+	int sertx_idx = 0;
+
+	if(memcmp(cmd, "\r\n", 2) == 0){
+		sertx_ptr[sertx_idx++] = 'O';
+		sertx_ptr[sertx_idx++] = 'K';
+	}
+
+	if(sertx_idx > 0){
+		sertx_ptr[sertx_idx++] = '\r';
+		sertx_ptr[sertx_idx++] = '\n';
+		ys_uart_send(sertx_idx);
+	}
+
+	return sertx_idx;
 }
